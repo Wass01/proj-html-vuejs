c726e6be85de8f1c4942178b7b9640f0180edbe5
wass01 <maatallawassim01@gmail.com>
2021-04-21 15:52:36 +0200

    prima parte header

---
+++ assets/js/dati.js
@@ -0,0 +1,23 @@
+const navbar = [
+  {
+    name: 'Home',
+  },
+  {
+    name: 'Culinary History',
+  },
+  {
+    name: 'Our Team',
+  },
+  {
+    name: 'Our Menu',
+  },
+  {
+    name: 'Takeout',
+  },
+  {
+    name: 'Bulletin',
+  },
+  {
+    name: 'Reservations',
+  },
+];
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 let app = new Vue({
   el: "#root",
   data: {
+    navbar: navbar,
   },
 
   methods: {
