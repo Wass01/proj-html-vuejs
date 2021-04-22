e96d39f102644ce2e0788f112e91b3c5f2c43ca2
wass01 <maatallawassim01@gmail.com>
2021-04-22 12:52:56 +0200

    fine second main

--- assets/js/dati.js
+++ assets/js/dati.js
@@ -25,14 +25,20 @@ const navbar = [
 const typesSushi = [
   {
     title: 'the best table in town',
+    number: '-1',
     descrition: 'Sed aenean egestas ut aliquam turpis mauris, molestie. Vitae tellus tempor sem id tempus neque, tellus turpis turpis. Morbi tortor id gravida aliquet.',
+    button: 'explore the menu',
   },
   {
     title: 'perfect for groups',
+    number: '-2',
     descrition: 'Quam eu proin sit massa condimentum. Volutpat non pulvinar aliquet nunc. Orci elementum in aliquet a gravida vivamus aliquam turpis vitae.',
+    button: 'make a reservation',
   },
   {
     title: 'fresh produce everyday',
+    number: '-3',
     descrition: 'Hendrerit amet, volutpat leo non, commodo maecenas scelerisque tincidunt. Morbi vulputate morbi purus quisque sit sagittis orci elementum gravida.',
+    button: 'learn more about us',
   },
 ];
